c285aa3392a5e2fd260dc4b2f5adc53ee67fd8f5
anhkhoavo0712 <anhkhoavo0712@gmail.com>
2023-05-23 05:40:59 +0700

    Convert all HTML files to Handlebars

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const expressHandlebars = require("express-handlebars");
+
 app.use(express.static(__dirname + "/public"));
+
+app.engine(
+  "hbs",
+  expressHandlebars.engine({
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+    extname: "hbs",
+    defaultLayout: "layout",
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/:page", (req, res) => {
+  res.render(req.params.page);
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
