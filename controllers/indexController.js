742afc98ab72b1d6d91c2de0c763bfe292936f16
anhkhoavo0712 <anhkhoavo0712@gmail.com>
2023-05-24 12:40:53 +0700

    Handle 404 and 500 error

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -6,8 +6,20 @@ controller.showHomepage = (req, res) => {
   res.render("index");
 };
 
-controller.showPage = (req, res) => {
-  res.render(req.params.page);
+controller.showPage = (req, res, next) => {
+  const pages = [
+    "cart",
+    "checkout",
+    "contact",
+    "login",
+    "my-account",
+    "product-detail",
+    "product-list",
+    "wishlist",
+  ];
+
+  if (pages.includes(req.params.page)) return res.render(req.params.page);
+  next();
 };
 
 module.exports = controller;
