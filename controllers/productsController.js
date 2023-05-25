0828bf646e634c3910328d72c64f395c83ea42bb
anhkhoavo0712 <anhkhoavo0712@gmail.com>
2023-05-25 08:47:18 +0700

    Add search and sort feature to Product list page

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,8 +1,10 @@
 const controller = {};
 
 const models = require("../models");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
-controller.show = async (req, res) => {
+controller.getData = async (req, res, next) => {
   // Get all categories with their belonged products
   const categories = await models.Category.findAll({
     include: [{ model: models.Product }],
@@ -19,12 +21,20 @@ controller.show = async (req, res) => {
   const tags = await models.Tag.findAll();
   res.locals.tags = tags;
 
+  next();
+};
+
+controller.show = async (req, res) => {
   // Get query
   const selectedCategory = isNaN(req.query.category)
     ? 0
     : parseInt(req.query.category);
   const selectedBrand = isNaN(req.query.brand) ? 0 : parseInt(req.query.brand);
   const selectedTag = isNaN(req.query.tag) ? 0 : parseInt(req.query.tag);
+  const searchKeyword = req.query.keyword || "";
+  const sort = ["price", "newest", "popular"].includes(req.query.sort)
+    ? req.query.sort
+    : "price";
 
   // Default filter
   const productFilter = {
@@ -42,11 +52,103 @@ controller.show = async (req, res) => {
         where: { id: selectedTag },
       },
     ];
+  if (searchKeyword.trim() != "") {
+    productFilter.where.name = { [Op.iLike]: `%${searchKeyword}%` };
+  }
+
+  switch (sort) {
+    case "newest":
+      productFilter.order = [["createdAt", "DESC"]];
+      break;
+    case "popular":
+      productFilter.order = [["stars", "DESC"]];
+      break;
+    default:
+      productFilter.order = [["price", "ASC"]];
+  }
+
+  res.locals.sort = sort;
+  res.locals.originalUrl = removeParam("sort", req.originalUrl);
+  if (Object.keys(req.query).length == 0) {
+    res.locals.originalUrl = res.locals.originalUrl + "?";
+  }
 
   const products = await models.Product.findAll(productFilter);
   res.locals.products = products;
 
   res.render("product-list");
 };
+
+controller.showDetail = async (req, res) => {
+  const id = isNaN(req.params.id) ? 0 : parseInt(req.params.id);
+  const product = await models.Product.findOne({
+    attributes: [
+      "id",
+      "name",
+      "stars",
+      "price",
+      "oldPrice",
+      "summary",
+      "description",
+      "specification",
+    ],
+    where: { id },
+    include: [
+      {
+        model: models.Image,
+        attributes: ["name", "imagePath"],
+      },
+      {
+        model: models.Review,
+        attributes: ["id", "review", "stars", "createdAt"],
+        include: [
+          {
+            model: models.User,
+            attributes: ["firstName", "lastName"],
+          },
+        ],
+      },
+      { model: models.Tag, attributes: ["id"] },
+    ],
+  });
+  res.locals.product = product;
+
+  const tagIds = [];
+  product.Tags.forEach((tag) => tagIds.push(tag.id));
+
+  const relatedProducts = await models.Product.findAll({
+    attributes: ["id", "name", "imagePath", "price", "oldPrice"],
+    include: [
+      {
+        model: models.Tag,
+        attributes: ["id"],
+        where: {
+          id: { [Op.in]: tagIds },
+        },
+      },
+    ],
+  });
+  res.locals.relatedProducts = relatedProducts;
+
+  res.render("product-detail");
+};
+
+function removeParam(key, sourceURL) {
+  var rtn = sourceURL.split("?")[0],
+    param,
+    params_arr = [],
+    queryString = sourceURL.indexOf("?") !== -1 ? sourceURL.split("?")[1] : "";
+  if (queryString !== "") {
+    params_arr = queryString.split("&");
+    for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+      param = params_arr[i].split("=")[0];
+      if (param === key) {
+        params_arr.splice(i, 1);
+      }
+    }
+    if (params_arr.length) rtn = rtn + "?" + params_arr.join("&");
+  }
+  return rtn;
+}
 
 module.exports = controller;
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/productsController");
 
-router.get("/", controller.show);
+router.get("/", controller.getData, controller.show);
+router.get("/:id", controller.getData, controller.showDetail);
 
 module.exports = router;
