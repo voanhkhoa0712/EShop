cbe6abb66abc0018577fded08849ae6e07f283bd
anhkhoavo0712 <anhkhoavo0712@gmail.com>
2023-05-30 11:25:09 +0700

    Using Render PostgresDB

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,12 +4,12 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/indexController");
 
-// router.get("/createTables", (req, res) => {
-//   let models = require("../models");
-//   models.sequelize.sync().then(() => {
-//     res.send("Tables created.");
-//   });
-// });
+router.get("/createTables", (req, res) => {
+  let models = require("../models");
+  models.sequelize.sync().then(() => {
+    res.send("Tables created.");
+  });
+});
 
 router.get("/", controller.showHomepage);
 
